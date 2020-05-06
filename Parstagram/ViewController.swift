7d22802227ff96fb6393a6c9b35bcfdbacbd6f45
trumpet-codes <trumpetbarks@gmail.com>
2020-05-05 20:44:00 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Jianling Su on 5/5/20.
+//  Copyright © 2020 magicTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
